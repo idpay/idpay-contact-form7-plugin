ba0806e6d0508ed8bc583f75bf1d7077ed7a0a2e
Meysam Razmi <m.razmi.92@gmail.com>
2020-07-13 16:36:39 +0430

    change tab character

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -13,24 +13,24 @@ namespace IDPay\CF7;
  */
 class Plugin {
 
-	/**
-	 * This is triggered when the plugin is going to be activated.
-	 *
-	 * Creates a table in database which stores all transactions.
-	 *
-	 * Also defines a variable in the 'wp-config.php' file so that
-	 * any contact form does not load javascript files in order to disabling
-	 * ajax capability of those form. This is happened so that we can redirect
-	 * to the gateway for processing a payment. => define('WPCF7_LOAD_JS',
-	 * false);
-	 */
-	public static function activate() {
-		global $wpdb;
-		$table_name = $wpdb->prefix . "cf7_transactions";
-		$version = get_option( 'idpay_cf7_version', '1.0' );
+    /**
+     * This is triggered when the plugin is going to be activated.
+     *
+     * Creates a table in database which stores all transactions.
+     *
+     * Also defines a variable in the 'wp-config.php' file so that
+     * any contact form does not load javascript files in order to disabling
+     * ajax capability of those form. This is happened so that we can redirect
+     * to the gateway for processing a payment. => define('WPCF7_LOAD_JS',
+     * false);
+     */
+    public static function activate() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "cf7_transactions";
+        $version = get_option( 'idpay_cf7_version', '1.0' );
 
-		if ( $wpdb->get_var( "show tables like '$table_name'" ) != $table_name ) {
-			$sql = "CREATE TABLE $table_name (
+        if ( $wpdb->get_var( "show tables like '$table_name'" ) != $table_name ) {
+            $sql = "CREATE TABLE $table_name (
                 id mediumint(11) NOT NULL AUTO_INCREMENT,
                 form_id bigint(11) DEFAULT '0' NOT NULL,
                 trans_id VARCHAR(255) NOT NULL,
@@ -44,56 +44,56 @@ class Plugin {
                 status VARCHAR(255) NOT NULL,
                 PRIMARY KEY id (id)
             );";
-			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-			dbDelta( $sql );
-		}
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta( $sql );
+        }
 
-		function wp_config_put( $slash = '' ) {
-			$config = file_get_contents( ABSPATH . "wp-config.php" );
+        function wp_config_put( $slash = '' ) {
+            $config = file_get_contents( ABSPATH . "wp-config.php" );
             $config = preg_replace( "/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config );
-			$config = preg_replace( "/^([\r\n\t ]*)(\<\?)(php)?/i", "<?php define('WPCF7_LOAD_JS', false);", $config );
-			file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
-		}
+            $config = preg_replace( "/^([\r\n\t ]*)(\<\?)(php)?/i", "<?php define('WPCF7_LOAD_JS', false);", $config );
+            file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
+        }
 
-		if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
-			wp_config_put();
-		} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
-			wp_config_put( '/' );
-		} else {
-			?>
+        if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
+            wp_config_put();
+        } else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
+            wp_config_put( '/' );
+        } else {
+            ?>
             <div class="error">
                 <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been activated.', 'idpay-contact-form-7' ); ?></p>
             </div>
-			<?php
-			exit;
-		}
+            <?php
+            exit;
+        }
 
-		$idpay_cf7_options = array(
-			'api_key' => '',
-			'return' => '',
-			'sandbox' => '1',
-			'currency' => 'rial',
-			'success_message' => __( 'Your payment has been successfully completed. Tracking code: {track_id}', 'idpay-contact-form-7' ),
-			'failed_message' => __( 'Your payment has failed. Please try again or contact the site administrator in case of a problem.', 'idpay-contact-form-7' ),
-		);
+        $idpay_cf7_options = array(
+            'api_key' => '',
+            'return' => '',
+            'sandbox' => '1',
+            'currency' => 'rial',
+            'success_message' => __( 'Your payment has been successfully completed. Tracking code: {track_id}', 'idpay-contact-form-7' ),
+            'failed_message' => __( 'Your payment has failed. Please try again or contact the site administrator in case of a problem.', 'idpay-contact-form-7' ),
+        );
 
-		add_option( "idpay_cf7_options", $idpay_cf7_options );
-	}
+        add_option( "idpay_cf7_options", $idpay_cf7_options );
+    }
 
-	/**
-	 * This is triggered when the plugin is going to be deactivated.
-	 */
-	public static function deactivate() {
+    /**
+     * This is triggered when the plugin is going to be deactivated.
+     */
+    public static function deactivate() {
 
-		function wp_config_delete( $slash = '' ) {
-			$config = file_get_contents( ABSPATH . "wp-config.php" );
-			$config = preg_replace( "/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config );
-			file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
-		}
+        function wp_config_delete( $slash = '' ) {
+            $config = file_get_contents( ABSPATH . "wp-config.php" );
+            $config = preg_replace( "/( ?)(define)( ?)(\()( ?)(['\"])WPCF7_LOAD_JS(['\"])( ?)(,)( ?)(0|1|true|false)( ?)(\))( ?);/i", "", $config );
+            file_put_contents( ABSPATH . $slash . "wp-config.php", $config );
+        }
 
-		function return_error() {
-			ob_start();
-			?>
+        function return_error() {
+            ob_start();
+            ?>
             <div class="error">
                 <p><?php _e( 'wp-config.php is not writable, please make wp-config.php writable - set it to 0777 temporarily, then set back to its original setting after this plugin has been deactivated.', 'idpay-contact-form-7' ); ?></p>
             </div>
@@ -103,22 +103,22 @@ class Plugin {
                     window.history.back();
                 }
             </script>
-			<?php
-			return ob_get_clean();
-		}
+            <?php
+            return ob_get_clean();
+        }
 
-		if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
-			wp_config_delete();
-		} else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
-			wp_config_delete( '/' );
-		} else {
-			print return_error();
-			exit;
-		}
+        if ( file_exists( ABSPATH . "wp-config.php" ) && is_writable( ABSPATH . "wp-config.php" ) ) {
+            wp_config_delete();
+        } else if ( file_exists( dirname( ABSPATH ) . "/wp-config.php" ) && is_writable( dirname( ABSPATH ) . "/wp-config.php" ) ) {
+            wp_config_delete( '/' );
+        } else {
+            print return_error();
+            exit;
+        }
 
-		delete_option( "idpay_cf7_options" );
-		delete_option( "idpay_cf7_version" );
-	}
+        delete_option( "idpay_cf7_options" );
+        delete_option( "idpay_cf7_version" );
+    }
 
     public static function update() {
         global $wpdb;
