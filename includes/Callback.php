3a1e76719a3c941010eb7a87c1404ec31a15f244
MohammadMalek <mimdeveloper.tv@gmail.com>
2022-06-08 13:14:23 +0430

    #master create Functions File Struct And Move any to this

--- includes/Callback.php
+++ includes/Callback.php
@@ -1,85 +1,5 @@
 <?php
-
-/**
- * Shows a configured message when a payment is successful.
- * This message can be configured at the Wordpress dashboard.
- * Also note that the message will be shown
- * if the short code has been inserted in a page.
- *
- * @see \IDPay\CF7\Admin\Menu::admin_table()
- *
- * @param $message
- * @param $track_id
- * @param $order_id
- *
- * @return string
- */
-function filled_message( $message, $track_id, $order_id ) {
-    return str_replace( [ "{track_id}", "{order_id}" ], [
-        $track_id,
-        $order_id,
-    ], $message );
-}
-
-function create_callback_response($db,$order_id,$trans_id,$track_id,$status,$message)
-{    $tableName = 'cf7_callbacks';
-    $row = $db->get_row( $db->prepare( "SELECT * FROM " . $db->prefix . $tableName ." WHERE trans_id='%s'", $trans_id ) );
-    if ( $row == NULL ) {
-        $row = [
-            'id' => $order_id,
-            'response' => json_encode([
-                'trans_id'=>$trans_id,
-                'track_id'=>$track_id,
-                'status'=>$status
-            ]),
-            'message' => json_encode($message),
-            'created_at' => time(),
-        ];
-        $db->insert( $db->prefix . $tableName, $row, array('%d','%s','%s') );
-    }
-    else {
-        $db->update( $db->prefix . $tableName,
-            array(
-                'response' => json_encode([
-                    'trans_id'=>$trans_id,
-                    'track_id'=>$track_id,
-                    'status'=>$status
-                ]),
-                'message' => json_encode($message),
-                'created_at' => time(),
-            ),
-            array( 'id' => $order_id ),
-            array(
-                '%s',
-            ),
-            array( '%d' )
-        );
-    }
-}
-
-/**
- * Calls the gateway endpoints.
- *
- * Tries to get response from the gateway for 4 times.
- *
- * @param $url
- * @param $args
- *
- * @return array|\WP_Error
- */
-function call_gateway_endpoint( $url, $args ) {
-    $number_of_connection_tries = 4;
-    while ( $number_of_connection_tries ) {
-        $response = wp_safe_remote_post( $url, $args );
-        if ( is_wp_error( $response ) ) {
-            $number_of_connection_tries --;
-            continue;
-        } else {
-            break;
-        }
-    }
-    return $response;
-}
+require_once( dirname(__FILE__) . '/Functions.php' );
 
 $status    = !empty($_POST['status'])  ? $_POST['status']   : (!empty($_GET['status'])  ? $_GET['status']   : NULL);
 $track_id  = !empty($_POST['track_id'])? $_POST['track_id'] : (!empty($_GET['track_id'])? $_GET['track_id'] : NULL);
@@ -274,7 +194,7 @@ if ( ! empty( $id ) && ! empty( $order_id ) ) {
             ),
             array( '%d' )
         );
-        // create callback log
+        // create callback log THIS
         create_callback_response(
             $wpdb,
             $verify_order_id,
